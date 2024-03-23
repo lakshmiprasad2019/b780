6ed5e49d7af181ee5a4b59ba09ead05e28f20b9d
lakshmiprasad2019 <madhava.sadm@gmail.com>
2024-03-23 10:21:21 +0530

    VPU addition terraform code

---
+++ mynewenv.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_vpc" "myvpc" {
+  cidr_block       = "10.10.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Myvpc"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.10.1.0/24"
+
+  tags = {
+    Name = "Public"
+  }
+}
