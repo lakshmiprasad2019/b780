451513fbabbcaf0365e3b206e9ee42d30c7be628
lakshmiprasad2019 <madhava.sadm@gmail.com>
2024-03-24 08:42:01 +0530

    new subnet has added

--- mynewenv.tf
+++ mynewenv.tf
@@ -16,4 +16,12 @@ resource "aws_subnet" "main" {
   tags = {
     Name = "Public"
   }
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.10.2.0/24"
+
+  tags = {
+    Name = "Private"
+  }
+
 }
